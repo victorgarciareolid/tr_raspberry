f0177525df209e153369ddbbf5890feef162b302
viktoriotw <victorgarciavilu@gmail.com>
2015-03-21 15:34:12 +0100

    Sends a new measurement

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 from requests.auth import HTTPDigestAuth
 import requests
-import hashlib
 import json
-import base64
 
 header = {'Authorization':'Digest username=%s, realm=%s, nonce=%s, uri=%s, response=%x'}
+parameter = {'board':'board34', 'sensor1': 2423, 'sensor2': 141241, 'sensor3': 123 }
 
+parameters = json.dumps(parameter, sort_keys=True)
 url = 'http://localhost:3000/newboard'
-
-r = requests.get(url, auth=HTTPDigestAuth('newboard', 'newboard'))
-print(r.json())
+#r = requests.get(url, auth=HTTPDigestAuth('newboard', 'newboard'))
+#print(r.json())
+r1 = requests.post('http://localhost:3000/', auth=HTTPDigestAuth('board34', 'board34julian'), data=parameters)
