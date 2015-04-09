9010b39ae6cc4dd1c4452b6fd5f451e753234c53
victorgarciavilu@gmail.com <victorgarciavilu@gmail.com>
2015-04-09 21:14:57 +0200

    Updateed

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ parameter = {'board':'board1', 'sensor1': 2423, 'location': {'lat': 40, 'lon': 2
 
 parameters = json.dumps(parameter, sort_keys=True)
  
-#url = 'http://localhost/newboard'
+#url = 'http://localhost:3000/newboard'
 #r = requests.get(url, auth=HTTPDigestAuth('newboard', 'newboard'))
 #print(r.json())
-r1 = requests.post('http://localhost/', auth=HTTPDigestAuth('board1', 'board1julian'), data=parameters, headers=header)
+r1 = requests.post('http://localhost:3000/', auth=HTTPDigestAuth('board1', 'board1julian'), data=parameters, headers=header)
